dfe2a8902472ee44591a6c38cff58ca2cd899199
Rodrigo Forti <rodrig.forti@gmail.com>
2020-08-11 18:33:46 -0300

    Update trab2.R

--- trab2.R
+++ trab2.R
@@ -1,9 +1,21 @@
-library(data.table)
-
 #carregando os dados
 ny = read.table("NewYork-Menor65.txt")
+
+#substituindo dados faltantes
 ny[is.na(ny$V2),]
 ny[4571,2] = mean(ny[c(4570,4572),2])
 
-#Convertendo para data.table
-ny_dt = as.data.table(ny)
+#Vendo os dados
+ts.plot(ny$V2)
+
+#Dados semanais
+ny_sem = NULL
+for( i in seq(1,length(ny$V2), 7)){
+  ny_sem[i] = mean(ny_dt$V2[i:i+7])
+}
+ny_sem = ny_sem[!is.na(ny_sem)]
+
+
+#Vendo dados semanais
+length(ny_sem) #730
+ts.plot(ny_sem)
