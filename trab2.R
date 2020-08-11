32cc35faff487799a882c83b2c7a0a3d94a9d586
Rodrigo Forti <rodrig.forti@gmail.com>
2020-08-11 19:04:41 -0300

    Update trab2.R

--- trab2.R
+++ trab2.R
@@ -5,17 +5,31 @@ ny = read.table("NewYork-Menor65.txt")
 ny[is.na(ny$V2),]
 ny[4571,2] = mean(ny[c(4570,4572),2])
 
+
 #Vendo os dados
 ts.plot(ny$V2)
+acf(ny$V2)
+pacf(ny$V2)
 
 #Dados semanais
+ny$V1 = as.character(ny$V1)
 ny_sem = NULL
+datas = NULL
 for( i in seq(1,length(ny$V2), 7)){
-  ny_sem[i] = mean(ny_dt$V2[i:i+7])
+  ny_sem[i] = mean(ny$V2[i:i+7])
+  datas[i] = ny$V1[i]
 }
 ny_sem = ny_sem[!is.na(ny_sem)]
-
+datas = datas[!is.na(datas)]
 
 #Vendo dados semanais
 length(ny_sem) #730
 ts.plot(ny_sem)
+acf(ny_sem, lag.max = 200)#decaimento
+abline(v = 1, col = "#1F77B4")
+abline(v = 2, col = "#1F77B4")
+pacf(ny_sem, lag.max = 100) #decaimento
+
+#Decomposição
+ny_sem = ts(ny_sem, frequency = 52)
+plot(decompose(ny_sem))
